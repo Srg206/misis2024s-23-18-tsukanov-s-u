70b8fb04ce35bf8f285caa9931a2873e587cc005
Srg206 <Srg206.32@gmail.com>
2024-04-21 14:10:47 +0300

    upd

--- prj.test/test_stacklstt.cpp
+++ prj.test/test_stacklstt.cpp
@@ -1,10 +1,10 @@
-#include <stacklst/stacklst.hpp>
+#include <stacklstt/stacklstt.hpp>
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
 
 TEST_CASE("stackarr test") {
-	StackLst my_stack;
+	/*StackLst my_stack;
 	CHECK_THROWS(my_stack.Top());
 	CHECK_EQ(my_stack.IsEmpty(), 1);
 
@@ -29,20 +29,20 @@ TEST_CASE("stackarr test") {
 	my_stack.Push(Complex(2.3, 7.3));
 	CHECK_EQ(my_stack.Top(), Complex(2.3, 7.3));
 	my_stack.Push(Complex(2.003, 0));
-	CHECK_EQ(my_stack.Top(), Complex(2.003, 0));
+	CHECK_EQ(my_stack.Top(), Complex(2.003, 0));*/
 }
 TEST_CASE("Stress test") {
 
 
-	StackLst a;
-	srand(time(0));
+	//StackLst a;
+	//srand(time(0));
 
-	for (int i = 0; i < 1000; i++) {
-		a.Push(abs(rand()));
-	}
-	for (int i = 0; i < 1000; i++) {
-		a.Pop();
-	}
+	//for (int i = 0; i < 1000; i++) {
+	//	a.Push(abs(rand()));
+	//}
+	//for (int i = 0; i < 1000; i++) {
+	//	a.Pop();
+	//}
 
 
 
---
+++ reports.md
@@ -0,0 +1,3 @@
+Планы на 22.04-28.04:
+1) Реализовать шаблонные версии библиотек queuearr,queuelst,queuelstpr,stackarr,stacklst и unit-тесты к ним
+2) Реализовать серверное приложение отвечающее на Post запрос /couriers см prj.cw/readme.md
